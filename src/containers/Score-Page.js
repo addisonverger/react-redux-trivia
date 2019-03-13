cd390b4208104ae713e11309fe05e48b3a8cb8de
Addison Verger <addison.j.verger@gmail.com>
2019-03-13 11:25:12 -0500

    fixed display for score page

--- src/containers/Score-Page.js
+++ src/containers/Score-Page.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import {Box} from 'react-bulma-components/full'
-import {Container} from 'react-bulma-components/full'
 import {Level} from 'react-bulma-components/full'
 import {Heading} from 'react-bulma-components/full'
 import {Button} from 'react-bulma-components/full'
@@ -11,38 +10,36 @@ const ScorePage = (props) => {
   return (
     <div>
       <Box className="m-auto">
-        <Container>
-          <Level>
-            <Level.Item>
-              <Heading size={1}>
-                Your Score
-              </Heading>
-            </Level.Item>
-          </Level>
-          <Level>
-            <Level.Item>
-              <Heading size={3}>
-                {props.score > 7 ? 'Great Job' : null}
-                {props.score > 5 && props.score < 8 ? 'Nice Work' : null}
-                {props.score < 6 ? 'Better Luck Next Time' : null}
-              </Heading>
-            </Level.Item>
-          </Level>
-          <Level>
-            <Level.Item>
-              <Heading size={2}>
-                {props.score} out of 10
-              </Heading>
-            </Level.Item>
-          </Level>
-          <Level className="questionNumber">
-            <Button type="button"
-                    className="is-link is-medium is-fullwidth"
-                    onClick={() => props.openPage('home-page')}>
-              New Game
-            </Button>
-          </Level>
-        </Container>
+        <Level>
+          <Level.Item>
+            <Heading size={1}>
+              Your Score
+            </Heading>
+          </Level.Item>
+        </Level>
+        <Level>
+          <Level.Item>
+            <Heading size={3}>
+              {props.score > 7 ? 'Great Job' : null}
+              {props.score > 5 && props.score < 8 ? 'Nice Work' : null}
+              {props.score < 6 ? 'Better Luck Next Time' : null}
+            </Heading>
+          </Level.Item>
+        </Level>
+        <Level>
+          <Level.Item>
+            <Heading size={2}>
+              {props.score} out of 10
+            </Heading>
+          </Level.Item>
+        </Level>
+        <Level className="questionNumber">
+          <Button type="button"
+                  className="is-link is-medium is-fullwidth"
+                  onClick={() => props.openPage('home-page')}>
+            New Game
+          </Button>
+        </Level>
       </Box>
     </div>
   )
