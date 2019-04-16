521f49a19c9f934d08226475b413ceaa08e7117b
Addison Verger <addison.j.verger@gmail.com>
2019-04-16 16:36:51 -0500

    added correct/incorrect icons for colorblindness

--- src/containers/Answers.js
+++ src/containers/Answers.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import {Tile} from 'react-bulma-components/full'
+import {Tile, Columns, Icon} from 'react-bulma-components/full'
 
 const Answers = (props) => {
   return (
@@ -14,7 +14,21 @@ const Answers = (props) => {
                   style={{cursor: 'pointer'}}
                   onClick={() => props.selectAnswer(index)}
                   notification>
-              <p>{props.decodeHtml(answer.text)}</p>
+              <Columns>
+                <Columns.Column size={11}>
+                  <p>{props.decodeHtml(answer.text)}</p>
+                </Columns.Column>
+                <Columns.Column size={1}>
+                  {answer.color === 'success' ?
+                  <Icon>
+                    <i className="fas fa-check"></i>
+                  </Icon> : ''}
+                  {answer.color === 'danger' ?
+                  <Icon>
+                    <i className="fas fa-times"></i>
+                  </Icon> : ''}
+                </Columns.Column>
+              </Columns>
             </Tile>
           )
         })}
